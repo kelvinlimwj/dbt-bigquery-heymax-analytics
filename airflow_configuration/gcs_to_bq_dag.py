14f1457bd72fb2f1edae70aaf35466ebb18d1a28
Kelvin Lim <kelvin.limwj@gmail.com>
2025-06-11 02:50:48 +0000

    Updated DAG configuration to pull from scripts folder

--- airflow_configuration/gcs_to_bq_dag.py
+++ airflow_configuration/gcs_to_bq_dag.py
@@ -1,30 +1,36 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'scripts')))
+
+from gcs_to_bq_utils import load_latest_csv_to_bq
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from gcs_to_bq_utils import load_latest_csv_to_bq
 
 default_args = {
     'owner': 'airflow',
-    'retries': 1,
+    'retries': 1
 }
 
 with DAG(
     dag_id='load_latest_gcs_csv_to_bigquery',
     default_args=default_args,
     start_date=datetime(2024, 1, 1),
-    schedule_interval='@daily',  # or '@once' for testing
+    schedule_interval='@daily',
     catchup=False,
-    tags=["gcs", "bigquery", "elt"]
+    tags=['gcs', 'bigquery']
 ) as dag:
 
     load_task = PythonOperator(
         task_id='load_latest_csv',
         python_callable=load_latest_csv_to_bq,
         op_kwargs={
-            "project_id": "heymax-kelvin-analytics",
-            "dataset_id": "heymax_analytics",
-            "table_id": "event_stream_raw",
-            "bucket_name": "heymax_kelvin_raw_data_sg",
+            "project_id": "your-project-id",
+            "dataset_id": "your-dataset-id",
+            "table_id": "your-table-name",
+            "bucket_name": "your-bucket-name",
             "prefix": "event_stream/"
         }
     )
